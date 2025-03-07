5fd3b8e19a811ed1a88aaef80a370c435c0af209
Narjes Taghlet <99217840+NarjesTaghlet@users.noreply.github.com>
2025-03-07 08:58:54 +0100

    Update init.sh

--- init.sh
+++ init.sh
@@ -5,6 +5,7 @@ sleep 60
 
 if [ -z "$DRUPAL_DB_HOST" ] || [ -z "$DRUPAL_DB_NAME" ] || [ -z "$DRUPAL_DB_USER" ] || [ -z "$DRUPAL_DB_PASSWORD" ]; then
   echo "Error: Missing required environment variables"
+  echo "DRUPAL_DB_HOST=[$DRUPAL_DB_HOST] DRUPAL_DB_NAME=[$DRUPAL_DB_NAME] DRUPAL_DB_USER=[$DRUPAL_DB_USER] DRUPAL_DB_PASSWORD=[$DRUPAL_DB_PASSWORD]"
   exit 1
 fi
 
@@ -13,23 +14,22 @@ echo "DB_NAME: $DRUPAL_DB_NAME"
 echo "DB_USER: $DRUPAL_DB_USER"
 echo "DB_PASSWORD: $DRUPAL_DB_PASSWORD"
 
-
-
 MAX_ATTEMPTS=60
 ATTEMPT=1
-until mysql -u"$DRUPAL_DB_USER" -p"$DRUPAL_DB_PASSWORD" "$DRUPAL_DB_NAME" -e "SELECT 1" 2>/tmp/mysql_error.log; do
+until mysql -h "$DRUPAL_DB_HOST" -u "$DRUPAL_DB_USER" -p"$DRUPAL_DB_PASSWORD" "$DRUPAL_DB_NAME" -e "SELECT 1" 2>/tmp/mysql_error.log; do
   echo "Attempt $ATTEMPT/$MAX_ATTEMPTS: Waiting for database connection..."
   cat /tmp/mysql_error.log
   sleep 2
   ATTEMPT=$((ATTEMPT + 1))
   if [ $ATTEMPT -gt $MAX_ATTEMPTS ]; then
     echo "Error: Database connection failed after $MAX_ATTEMPTS attempts."
+    cat /tmp/mysql_error.log
     exit 1
   fi
 done
 
 echo "Database connection successful!"
-# Vérifier le montage du volume (avant la connexion DB)
+# Vérifier le montage du volume
 echo "Checking volume mount..."
 if ! mount | grep -q "/var/www/html/web/sites/default"; then
   echo "Error: Azure File Share volume not mounted on /var/www/html/web/sites/default!"
@@ -62,14 +62,13 @@ EOL
 
   cd /var/www/html
   composer require drush/drush --prefer-dist || { echo "Failed to install Drush"; exit 1; }
-  # Installer Drupal avec ~/.my.cnf
+  # Installer Drupal
   vendor/bin/drush site:install standard \
     --account-name="${DRUPAL_ADMIN_USERNAME:-admin}" \
     --account-pass="${DRUPAL_ADMIN_PASSWORD:-admin}" \
     --site-name="${DRUPAL_SITE_NAME:-Drupal 10 Site}" \
     --no-interaction \
     -y || { echo "Drush install failed"; exit 1; }
-
 
   chmod 444 /var/www/html/web/sites/default/settings.php || { echo "Failed to chmod settings.php post-install"; exit 1; }
   chown -R www-data:www-data /var/www/html/web/sites/default/files || { echo "Failed to chown files directory"; exit 1; }
